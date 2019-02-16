7010eda5f7ee1a5a3d6708909ec0e84edc49e988
mark-williams <markwill@hotmail.co.uk>
2019-02-16 11:15:20 +0000

    feat: simple counter component

---
+++ src/counter/counter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Counter extends React.Component {
+  state = { count: 0 };
+
+  onClick = () => this.setState(state => ({ count: state.count + 1 }));
+  render = () => {
+    const { count } = this.state;
+    <button onClick={this.onClick}>{count}</button>;
+  };
+}
+
+export default Counter;
