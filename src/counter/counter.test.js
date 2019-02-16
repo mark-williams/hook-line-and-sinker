4a18a940e28a6accaa202b87bcc2a536df7f5e99
mark-williams <markwill@hotmail.co.uk>
2019-02-16 11:37:34 +0000

    chore: set up enzyme and add tests for counter component

--- src/counter/counter.test.js
+++ src/counter/counter.test.js
@@ -1,2 +1,25 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import Counter from './counter';
+
+const getDisplayedCount = wrapper => wrapper.find('button').text();
+
+describe('counter', () => {
+  describe('when initially rendered', () => {
+    const wrapper = shallow(<Counter />);
+    it('should display count of zero', () => {
+      expect(getDisplayedCount(wrapper)).toContain('0');
+    });
+  });
+
+  describe('when clicked', () => {
+    const wrapper = shallow(<Counter />);
+    it(' displays incremented count', () => {
+      const button = wrapper.find('button');
+      expect(button.text()).toContain('0');
+
+      button.simulate('click');
+      expect(getDisplayedCount(wrapper)).toContain('1');
+    });
+  });
+});
