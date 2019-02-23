2a063bbdcaf65350d73eabcc431046e0f925be07
mark-williams <markwill@hotmail.co.uk>
2019-02-23 10:27:36 +0000

    chore: form now submits (admittedly just alerts its values!)

--- src/complex-form/complex-form.js
+++ src/complex-form/complex-form.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { FormWrapper, FormItem, FormButtons } from './style';
 
-const ComplexForm = () => {
+const useForm = () => {
   const [firstName, setFirstName] = useState('');
   const [secondName, setSecondName] = useState('');
   const [email, setEmail] = useState('');
@@ -27,41 +27,61 @@ const ComplexForm = () => {
     }
   };
 
+  return {
+    values: {
+      firstName,
+      secondName,
+      email
+    },
+    onChange
+  };
+};
+
+const ComplexForm = () => {
+  const { values, onChange } = useForm();
+
+  const onSubmit = () => {
+    // eslint-disable-next-line no-alert
+    alert('Submitting: ' + JSON.stringify(values, null, 2));
+  };
+
   return (
     <FormWrapper>
-      <FormItem>
-        <label htmlFor="first-name">First name</label>
-        <input
-          id="first-name"
-          name="firstName"
-          type="text"
-          value={firstName}
-          onChange={onChange}
-        />
-      </FormItem>
-      <FormItem>
-        <label htmlFor="second-name">Second name</label>
-        <input
-          id="second-name"
-          name="secondName"
-          type="text"
-          value={secondName}
-          onChange={onChange}
-        />
-      </FormItem>
-      <FormItem>
-        <label htmlFor="email">Email</label>
-        <input
-          id="email"
-          name="email"
-          type="text"
-          value={email}
-          onChange={onChange}
-        />
-      </FormItem>
-      <FormButtons>
-        <button type="submit">Submit</button>
-      </FormButtons>
+      <form onSubmit={onSubmit}>
+        <FormItem>
+          <label htmlFor="first-name">First name</label>
+          <input
+            id="first-name"
+            name="firstName"
+            type="text"
+            value={values.firstName}
+            onChange={onChange}
+          />
+        </FormItem>
+        <FormItem>
+          <label htmlFor="second-name">Second name</label>
+          <input
+            id="second-name"
+            name="secondName"
+            type="text"
+            value={values.secondName}
+            onChange={onChange}
+          />
+        </FormItem>
+        <FormItem>
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            name="email"
+            type="text"
+            value={values.email}
+            onChange={onChange}
+          />
+        </FormItem>
+        <FormButtons>
+          <button type="submit">Submit</button>
+        </FormButtons>
+      </form>
     </FormWrapper>
   );
 };
