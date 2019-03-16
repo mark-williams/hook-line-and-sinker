9d884260d56cdf9f7cc7935dfc7f62540a59fb8d
mark-williams <markwill@hotmail.co.uk>
2019-03-16 11:38:40 +0000

    chore: implement simple search using 'useEffect'

--- src/article-search/article-search.js
+++ src/article-search/article-search.js
@@ -1,10 +1,29 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+const Article = styled.div`
+  margin-bottom: 0.4rem;
+`;
 
 const ArticleSearch = () => {
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    fetch('https://hn.algolia.com/api/v1/search?query=Liverpool')
+      .then(response => response.json())
+      .then(data => {
+        setArticles(data.hits);
+      });
+  }, []);
+
   return (
     <Fragment>
       <h1>Article Search</h1>
       <h3>Here are a list of articles matching your search:</h3>
+      <div>
+        {articles.map(a => (
+          <Article key={a.objectID}>{a.title}</Article>
+        ))}
+      </div>
     </Fragment>
   );
 };
